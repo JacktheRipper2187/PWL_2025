154b8a47905cbf80cc1a76aaf86f33143cb1dad2
Ulil Fahmi <ulilf442@gmail.com>
2025-02-17 16:49:53 +0700

    View Laravel - Jobsheet 1

--- routes/web.php
+++ routes/web.php
@@ -113,3 +113,9 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+// Route::get('/greeting', function (){
+//     return view('blog.hello', ['name' => 'Ulil']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
